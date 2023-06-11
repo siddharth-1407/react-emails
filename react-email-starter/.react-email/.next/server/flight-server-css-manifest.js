5798f0614606dbcde10ba4b60384d87ad17aa838
siddharth-1407 <sanksiddharth@gmail.com>
2023-06-11 22:57:29 +0530

    initial commit

---
+++ react-email-starter/.react-email/.next/server/flight-server-css-manifest.js
@@ -0,0 +1,24 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\app\\page": [
+      "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\styles\\globals.css",
+      "D:\\VS Code\\React emails\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ],
+    "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\app\\preview\\[slug]\\page": [
+      "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\styles\\globals.css",
+      "D:\\VS Code\\React emails\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}"
+    ]
+  },
+  "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\app\\page.tsx": [
+    "D:\\VS Code\\React emails\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ],
+  "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\app\\layout.tsx": [
+    "D:\\VS Code\\React emails\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ],
+  "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\app\\preview\\[slug]\\page.tsx": [
+    "D:\\VS Code\\React emails\\react-email-starter\\node_modules\\next\\font\\google\\target.css?{\"path\":\"src\\\\app\\\\layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"],\"variable\":\"--font-inter\"}],\"variableName\":\"inter\"}",
+    "D:\\VS Code\\React emails\\react-email-starter\\.react-email\\src\\styles\\globals.css"
+  ]
+}
